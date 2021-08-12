2624e945f8ffe68ec99eae7db09cce7981e25f3e
Tiago7Felipe <tiiago.felipe@gmail.com>
2021-08-12 14:20:52 -0300

    Meu primeiro commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,12 @@
+/// <reference types = "cypress"/>
+
+context('Funcionalidade login', () => {
+    it('Login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+    })
+});
