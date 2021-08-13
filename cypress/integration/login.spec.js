f9715ba846049c80a50287453b01233ba5cea7fa
Tiago7Felipe <tiiago.felipe@gmail.com>
2021-08-12 21:14:37 -0300

    Atualizando os casos de teste

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -9,4 +9,20 @@ context('Funcionalidade login', () => {
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
     })
+
+    it ('Login com senha inválida', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('kagsgdmghfnn')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+
+    it.only('Login com email Inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário')
+    })
 });
