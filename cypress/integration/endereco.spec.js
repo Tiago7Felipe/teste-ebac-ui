b86f3b636ff90b34f67d774b75d62c9c2c7ceb90
Tiago7Felipe <tiiago.felipe@gmail.com>
2021-08-19 16:55:24 -0300

    Alterações arquivos

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types = 'cypress' />
-
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require ('../fixtures/endereco.json')
 
 describe('Funcionalidade endereços - endereço faturamento', () => {
     beforeEach(() => {
@@ -10,7 +11,26 @@ describe('Funcionalidade endereços - endereço faturamento', () => {
     });
 
     it('Deve realizar o cadastro do endereçe de faturamento com sucesso', () => {
-        //login
+        EnderecoPage.editarEnderecoFaturamento('André', 'Antonio', 'Itau','Brasil','rua A', '555', 'Bonfim','minas','30120-040', '3198653212','teste@teste.com.br')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+        
+    });
+
+    it.only('Deve realizar o cadastro do endereço de faturamento com sucesso - arquivo', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].rua,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].uf,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
         
     });
 });
